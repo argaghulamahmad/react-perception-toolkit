63f57e31f1ac59609450998523cd1784f1ce999a
Arga Ghulam Ahmad <arga.ahmad@tokopedia.com>
2019-06-12 16:33:10 +0700

    Refactor Code Convert default function syntax to arrow function.

--- src/components/Scanner/Scanner.js
+++ src/components/Scanner/Scanner.js
@@ -1,98 +1,102 @@
 import React, {Component} from 'react';
 
-import initPerceptionToolKit from '../../initPerceptionToolkit';
+import importPerceptionToolKit from '../../initPerceptionToolkit';
 
 class Scanner extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            targetText: 'test'
+        }
+    }
+
     componentDidMount() {
-        initPerceptionToolKit();
+        importPerceptionToolKit();
 
-        async function init() {
-            const cardContainer = document.body.querySelector('.container');
+        const cardContainer = document.body.querySelector('.container');
+        const startButton = document.getElementById('get-started');
 
+        console.log('didMount', this);
+
+        const addEventListeners = () => {
+            const {Events, Elements} = window.PerceptionToolkit;
+
+            window.addEventListener(Events.PerceivedResults, (evt) => {
+                const {found, lost} = evt.detail;
+
+                window.evt = evt;
+
+                if (found.length !== 0) {
+                    const notRecognizedCard =
+                        cardContainer.querySelector('[data-not-recognized]');
+                    if (notRecognizedCard) {
+                        notRecognizedCard.remove();
+                    }
+                }
+
+                if (cardContainer.childNodes.length > 0) {
+                    return;
+                }
+
+                if (found.length === 0 && found.length === 0) {
+                    evt.preventDefault();
+
+                    const card = new Elements.Card();
+                    card.src = 'Sorry, this item was not recognized.';
+                    card.dataset.notRecognized = true;
+                    cardContainer.appendChild(card);
+                    return;
+                }
+
+                if (found.length > 0) {
+                    evt.preventDefault();
+
+                    const card = new Elements.Card();
+                    card.src = found[0].content;
+                    cardContainer.appendChild(card);
+
+                    this.setState({
+                        targetText: found[0].artifact.arTarget[0].text
+                    });
+
+                    const button = new Elements.ActionButton();
+                    button.label = 'Find on Tokopedia';
+                    button.addEventListener('click', () => {
+                        let url = "https://www.tokopedia.com/search?st=product&q=";
+                        let productName = found[0].content.name;
+                        let searchUrl = url + productName;
+                        window.open(searchUrl, '_blank');
+                        card.close();
+                    });
+                    card.appendChild(button);
+
+                    return;
+                }
+            });
+
+            window.addEventListener(Events.CameraAccessDenied, () => {
+                /*TODO use our own created component*/
+            });
+
+            window.addEventListener(Events.DeviceNotSupported, () => {
+                /*TODO use our own created component*/
+            });
+        };
+
+        const init = async () => {
             window.PerceptionToolkit = window.PerceptionToolkit || {};
             window.PerceptionToolkit.config = {
                 debugLevel: 'verbose',
-
                 detectionMode: 'active',
-
-                button: document.getElementById('get-started'),
-
+                button: startButton,
                 cardContainer: cardContainer,
-
                 onboarding: false,
-
                 artifactSources: [],
-
                 onload() {
                     addEventListeners();
                 }
             };
-
-            function addEventListeners() {
-                const {Events, Elements} = window.PerceptionToolkit;
-
-                window.addEventListener(Events.PerceivedResults, (evt) => {
-                    const {found, lost} = evt.detail;
-
-                    window.evt = evt;
-
-                    if (found.length !== 0) {
-                        const notRecognizedCard =
-                            cardContainer.querySelector('[data-not-recognized]');
-                        if (notRecognizedCard) {
-                            notRecognizedCard.remove();
-                        }
-                    }
-
-                    if (cardContainer.childNodes.length > 0) {
-                        return;
-                    }
-
-                    if (found.length === 0 && found.length === 0) {
-                        evt.preventDefault();
-
-                        const card = new Elements.Card();
-                        card.src = 'Sorry, this item was not recognized.';
-                        card.dataset.notRecognized = true;
-                        cardContainer.appendChild(card);
-                        return;
-                    }
-
-                    if (found.length > 0) {
-                        evt.preventDefault();
-
-                        const card = new Elements.Card();
-                        card.src = found[0].content;
-                        cardContainer.appendChild(card);
-
-                        const button = new Elements.ActionButton();
-                        button.label = 'Find on Tokopedia';
-                        button.addEventListener('click', () => {
-                            console.log(found);
-                            console.log('clicked');
-                            let url = "https://www.tokopedia.com/search?st=product&q=";
-                            let productName = found[0].content.name;
-                            let searchUrl = url + productName;
-                            window.open(searchUrl,'_blank');
-                            card.close();
-                        });
-                        card.appendChild(button);
-
-                        return;
-                    }
-                });
-
-                window.addEventListener(Events.CameraAccessDenied, () => {
-                    const card = new Elements.Card();
-                    card.src = 'Camera unavailable or access denied';
-                    cardContainer.appendChild(card);
-                });
-
-                window.addEventListener(Events.DeviceNotSupported, () => {
-                    console.warn('Device Not Supported');
-                });
-            }
-        }
+        };
 
         init();
     }
@@ -103,6 +107,7 @@ class Scanner extends Component {
                 <h2>Scanner</h2>
                 <div className="container"/>
                 <button id="get-started">Get started</button>
+                <p>{this.state.targetText}</p>
             </div>
         );
     }
