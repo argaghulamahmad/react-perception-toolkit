e88500feb62de9c7480a16ede82141c06be63600
Arga Ghulam Ahmad <arga.ahmad@tokopedia.com>
2019-06-13 13:44:28 +0700

    Disable some function for no artifact purpose

--- src/components/Scanner/perception-toolkit/lib/perception-toolkit/bootstrap.js
+++ src/components/Scanner/perception-toolkit/lib/perception-toolkit/bootstrap.js
@@ -1,25 +1,10 @@
-/**
- * @license
- * Copyright 2019 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-import { ActionButton, Card } from '../src/elements/index.js';
-import { DeviceSupport } from '../src/support/device-support.js';
-import { GetUserMediaSupport } from '../src/support/get-user-media.js';
-import { WasmSupport } from '../src/support/wasm.js';
-import { fire } from '../src/utils/fire.js';
-import { cameraAccessDenied, captureClosed, captureStopped, markerChanges } from './events.js';
+/*import { ActionButton, Card } from '../src/elements/index.js';*/
+import {DeviceSupport} from '../src/support/device-support.js';
+import {GetUserMediaSupport} from '../src/support/get-user-media.js';
+import {WasmSupport} from '../src/support/wasm.js';
+import {fire} from '../src/utils/fire.js';
+import {cameraAccessDenied, captureClosed, captureStopped, markerChanges} from './events.js';
+
 const deviceNotSupported = 'pt.devicenotsupported';
 window.PerceptionToolkit.config = window.PerceptionToolkit.config || {};
 // Expose events.
@@ -30,11 +15,13 @@ window.PerceptionToolkit.Events = {
     DeviceNotSupported: deviceNotSupported,
     MarkerChanges: markerChanges,
 };
+
 // Expose elements.
-window.PerceptionToolkit.Elements = {
+/*window.PerceptionToolkit.Elements = {
     ActionButton,
     Card
-};
+};*/
+
 // Expose functions.
 window.PerceptionToolkit.Functions = {
     initializeExperience,
@@ -42,15 +29,17 @@ window.PerceptionToolkit.Functions = {
         // Replaced when main.ts has loaded.
     }
 };
+
 if (window.PerceptionToolkit.config.onload) {
     window.PerceptionToolkit.config.onload.call(null);
 }
+
 /**
  * Perform a device support test, then load the loader & onboarding.
  */
 const load = new Promise(async (resolve) => {
-    const { config } = window.PerceptionToolkit;
-    const { showLoaderDuringBoot = true } = config;
+    const {config} = window.PerceptionToolkit;
+    const {showLoaderDuringBoot = true} = config;
     // Detect the necessary support.
     const deviceSupport = new DeviceSupport();
     deviceSupport.addDetector(GetUserMediaSupport);
@@ -59,7 +48,7 @@ const load = new Promise(async (resolve) => {
     // If everything necessary is supported, inject the loader and show it if
     // desired.
     if (support[GetUserMediaSupport.name] && support[WasmSupport.name]) {
-        const { showLoader } = await import('./loader.js');
+        const {showLoader} = await import('./loader.js');
         // Only show the loader if requested.
         if (showLoaderDuringBoot) {
             showLoader();
@@ -69,11 +58,11 @@ const load = new Promise(async (resolve) => {
             await import('./onboarding.js');
         }
         resolve(true);
-    }
-    else {
+    } else {
         resolve(false);
     }
 });
+
 /**
  * Initialize the experience.
  */
@@ -81,31 +70,36 @@ async function initializeExperience() {
     console.log('initialize experience', this);
     const supported = await load;
     if (!supported) {
-        const { hideLoader } = await import('./loader.js');
-        hideLoader();
+        /*const {hideLoader} = await import('./loader.js');
+        hideLoader();*/
         const deviceNotSupportedEvt = fire(deviceNotSupported, window);
         if (!deviceNotSupportedEvt.defaultPrevented) {
+            /*
+            TODO create method add this info to the scanner component
             addCardToPage({
                 cls: 'no-support',
                 msg: 'Sorry, this browser does not support the required features',
-            });
+            });*/
         }
         return;
     }
-    const { showLoader, hideLoader } = await import('./loader.js');
-    const { config } = window.PerceptionToolkit;
-    const { sitemapUrl, detectionMode = 'passive' } = config;
+    const {showLoader, hideLoader} = await import('./loader.js');
+    const {config} = window.PerceptionToolkit;
+    const {sitemapUrl, detectionMode = 'passive'} = config;
     if (config && config.onboardingImages && config.onboarding) {
         hideLoader();
-        const { startOnboardingProcess } = await import('./onboarding.js');
+        const {startOnboardingProcess} = await import('./onboarding.js');
         await startOnboardingProcess(config.onboardingImages);
     }
     showLoader();
-    const { initialize, close } = await import('./main.js');
+    const {initialize, close} = await import('./main.js');
     // Now the experience is inited, update the closeExperience fn.
     window.PerceptionToolkit.Functions.closeExperience = close;
-    initialize({ detectionMode, sitemapUrl });
+    initialize({detectionMode, sitemapUrl});
 }
+
+/*
+TODO modify this function so this method has functionality to send info to the scanner component
 function addCardToPage({ msg = '', cls = '' }) {
     if (!customElements.get(Card.defaultTagName)) {
         customElements.define(Card.defaultTagName, Card);
@@ -121,14 +115,15 @@ function addCardToPage({ msg = '', cls = '' }) {
     card.src = msg;
     cardContainer.appendChild(card);
     return;
-}
+}*/
+
 // Bootstrap.
 (async function () {
     console.log('bootstrap', this);
     const supported = await load;
-    const { hideLoader, showLoader } = await import('./loader.js');
-    const { config } = window.PerceptionToolkit;
-    const { buttonVisibilityClass = 'visible' } = config;
+    const {hideLoader, showLoader} = await import('./loader.js');
+    const {config} = window.PerceptionToolkit;
+    const {buttonVisibilityClass = 'visible'} = config;
     const getStarted = config.button ? config.button :
         config.buttonSelector ? document.body.querySelector(config.buttonSelector) :
             null;
@@ -142,10 +137,12 @@ function addCardToPage({ msg = '', cls = '' }) {
         // If the button was visible and the user clicked it, show the no support
         // card here.
         if (!supported) {
+            /*
+            TODO create method add this info to the scanner component
             addCardToPage({
                 cls: 'no-support',
                 msg: 'Sorry, this browser does not support the required features',
-            });
+            });*/
             return;
         }
         showLoader();
