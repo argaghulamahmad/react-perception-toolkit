b285d7ef347014d9085581ddeb63231c8f4630d2
Arga Ghulam Ahmad <arga.ahmad@tokopedia.com>
2019-06-14 15:47:16 +0700

    Remove unused code Deleted addEventListeners function and change some perception toolkit config.

--- src/components/Scanner/Scanner.js
+++ src/components/Scanner/Scanner.js
@@ -15,86 +15,15 @@ class Scanner extends Component {
 
         console.log('didMount', this);
 
-        const addEventListeners = () => {
-            console.log('addEventListeners', this);
-
-            const {Events, Elements} = window.PerceptionToolkit;
-
-            window.addEventListener(Events.PerceivedResults, (evt) => {
-                console.log('evt', this);
-
-                const {found, lost} = evt.detail;
-
-                window.evt = evt;
-
-                if (found.length !== 0) {
-                    const notRecognizedCard =
-                        cardContainer.querySelector('[data-not-recognized]');
-                    if (notRecognizedCard) {
-                        notRecognizedCard.remove();
-                    }
-                }
-
-                if (cardContainer.childNodes.length > 0) {
-                    return;
-                }
-
-                if (found.length === 0 && found.length === 0) {
-                    evt.preventDefault();
-
-                    const card = new Elements.Card();
-                    card.src = 'Sorry, this item was not recognized.';
-                    card.dataset.notRecognized = true;
-                    cardContainer.appendChild(card);
-                    return;
-                }
-
-                if (found.length > 0) {
-                    evt.preventDefault();
-
-                    const card = new Elements.Card();
-                    card.src = found[0].content;
-                    cardContainer.appendChild(card);
-
-                    this.setState({
-                        targetText: found[0].artifact.arTarget[0].text
-                    });
-
-                    const button = new Elements.ActionButton();
-                    button.label = 'Find on Tokopedia';
-                    button.addEventListener('click', () => {
-                        let url = "https://www.tokopedia.com/search?st=product&q=";
-                        let productName = found[0].content.name;
-                        let searchUrl = url + productName;
-                        window.open(searchUrl, '_blank');
-                        card.close();
-                    });
-                    card.appendChild(button);
-
-                    return;
-                }
-            });
-
-            window.addEventListener(Events.CameraAccessDenied, () => {
-                /*TODO use our own created component*/
-            });
-
-            window.addEventListener(Events.DeviceNotSupported, () => {
-                /*TODO use our own created component*/
-            });
-        };
-
         const initScanner = async () => {
             window.PerceptionToolkit = window.PerceptionToolkit || {};
             window.PerceptionToolkit.config = {
                 debugLevel: 'verbose',
-                detectionMode: 'passive',
                 button: startButton,
                 cardContainer: cardContainer,
-                artifactSources: [],
                 callback: openUrl,
                 onload() {
-                    addEventListeners();
+                    console.log('initScanner')
                 }
             };
         };
@@ -104,8 +33,8 @@ class Scanner extends Component {
                 .then(module => {
                     window.BarcodeDetector = class CustomBarcodeDetectorPolyfill extends module.BarcodeDetectorPolyfill {
                         constructor() {
-                            // hardcode the script for the worker to run for now
-                            // TODO: add webpack worker loader to handle dynamically named worker script
+                            /*Hardcoded the script for the worker to run for now.
+                            TODO: add webpack worker loader to handle dynamically named worker script*/
                             super('barcode-detector_worker.js');
                         }
                     }
