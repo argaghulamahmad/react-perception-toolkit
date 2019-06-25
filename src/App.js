cfdb328675540a48d3e7e6acf79c8fcd65e6e572
Arga Ghulam Ahmad <arga.ahmad@tokopedia.com>
2019-06-25 15:21:32 +0700

    Add link and route config to home and scanner component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, {Component} from 'react';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Link, Route} from 'react-router-dom';
 
 import './App.css';
 import Scanner from "./components/Scanner/Scanner";
+import Home from "./components/Home/Home";
 
 class App extends Component {
     render() {
         return (
             <BrowserRouter>
                 <div className="App">
-                    <Scanner/>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/scanner/">Scanner</Link>
+                            </li>
+                        </ul>
+                    </nav>
+
+                    <Route path="/" exact component={Home} />
+                    <Route path="/scanner/" component={Scanner} />
                 </div>
             </BrowserRouter>
         );
