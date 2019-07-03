7b7cb0bb1fdaa481bc85d0671aed69ea92214bff
Arga Ghulam Ahmad <arga.ahmad@tokopedia.com>
2019-07-03 13:26:55 +0700

    Side by side scanner js and wasm

---
+++ src/Demo.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './App.css';
+import Benchmark from "./components/Benchmark/Benchmark";
+
+class Demo extends Component {
+    render() {
+        return (
+            <div>
+                <Benchmark/>
+            </div>
+        );
+    }
+}
+
+export default Demo;
--- src/components/Benchmark/Benchmark.js
+++ src/components/Benchmark/Benchmark.js
@@ -1,16 +1,17 @@
 import React, {Component} from 'react';
 import Scanner from "../Scanner/Scanner";
+import ScannerJs from "../ScannerJs/ScannerJs";
 import "./Benchmark.css"
 
 class Benchmark extends Component {
     render() {
         return (
             <div>
-                <div>
+                <div className={"split left"}>
                     <Scanner/>
                 </div>
-                <div>
-                    Hello
+                <div className={"split right"}>
+                    <ScannerJs/>
                 </div>
             </div>
         );
