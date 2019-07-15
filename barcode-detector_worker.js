c59a15a74efd28342bccd9a599ccc1390bdfe151
Arga Ghulam Ahmad <arga.ahmad@tokopedia.com>
2019-07-15 15:04:44 +0700

    Updates

---
+++ barcode-detector_worker.js
@@ -0,0 +1,48 @@
+(function () {
+  'use strict';
+
+
+  function getDetectorModule() {
+      return self.Module;
+  }
+  class WasmBarcodeDetector {
+      process(data) {
+          const Module = getDetectorModule();
+          const fileData = data.data;
+          const buffer = Module._malloc(fileData.length);
+          Module.HEAPU8.set(fileData, buffer);
+          const result = Module.readBarcodeFromPng(data.data, data.width, data.height);
+          Module._free(buffer);
+          if (result.text) {
+              return [{ rawValue: result.text }];
+          }
+          return [];
+      }
+  }
+  const detector = new WasmBarcodeDetector();
+  self.onmessage = (e) => {
+      // Initializing.
+      if (typeof e.data === 'string') {
+          const pathPrefix = e.data;
+          self.Module = {
+              locateFile(url) {
+                  if (url.endsWith('.wasm')) {
+                      return `${pathPrefix}third_party/zxing/${url}`;
+                  }
+                  return url;
+              },
+              onRuntimeInitialized() {
+                  self.postMessage('ready');
+              }
+          }; // Cast as WasmModule because the import will augment.
+          if ('importScripts' in self) {
+              // Import the emscripten'd file that loads the wasm.
+              importScripts(`${pathPrefix}third_party/zxing/zxing_reader.js`);
+          }
+          return;
+      }
+      const data = detector.process(e.data);
+      self.postMessage(data);
+  };
+
+}());
---
+++ precache-manifest.207f192561b925add784234d2d0dcb80.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be7f177a923e6e9d9d3963e25fa31dbe",
+    "url": "/argaghulamahmad/react-perception-toolkit/110772a404455befea77.worker.js"
+  },
+  {
+    "revision": "186de181fc0d380a1469f4b12e1fc4cf",
+    "url": "/argaghulamahmad/react-perception-toolkit/index.html"
+  },
+  {
+    "revision": "5a5ca705bf741e2d064a",
+    "url": "/argaghulamahmad/react-perception-toolkit/static/css/main.89a17ff6.chunk.css"
+  },
+  {
+    "revision": "d9c42bac7def2cd060bf",
+    "url": "/argaghulamahmad/react-perception-toolkit/static/js/2.7640e5b1.chunk.js"
+  },
+  {
+    "revision": "6f63a042feace6b31ab7",
+    "url": "/argaghulamahmad/react-perception-toolkit/static/js/3.637c90a3.chunk.js"
+  },
+  {
+    "revision": "e2bbf95f26e890bd9e0b",
+    "url": "/argaghulamahmad/react-perception-toolkit/static/js/4.56200746.chunk.js"
+  },
+  {
+    "revision": "5a5ca705bf741e2d064a",
+    "url": "/argaghulamahmad/react-perception-toolkit/static/js/main.7260fb77.chunk.js"
+  },
+  {
+    "revision": "212622c2e797464fcad7",
+    "url": "/argaghulamahmad/react-perception-toolkit/static/js/runtime~main.ecb77edf.js"
+  }
+]);
